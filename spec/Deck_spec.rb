80d3a86baea7ab58b4cbcc852ba4c0088696743c
Yasir Shoaib <yasirshoaib@hotmail.com>
2016-01-10 21:11:02 -0500

    add more tests

--- spec/Deck_spec.rb
+++ spec/Deck_spec.rb
@@ -14,4 +14,20 @@ describe Combo::Deck do
   it "should contain King of Hearts" do
     expect(deck.to_a).to include(Combo::Card.new(:King, :Hearts, 10))
   end
+
+  it "should contain Ace of Spades" do
+    expect(deck.to_a).to include(Combo::Card.new(:Ace, :Spades, 1))
+  end
+
+  it "should contain Jack of Diamonds" do
+    expect(deck.to_a).to include(Combo::Card.new(:Jack, :Diamonds, 10))
+  end
+
+  it "should contain Nine of Clubs" do
+    expect(deck.to_a).to include(Combo::Card.new(:Nine, :Clubs, 9))
+  end
+
+  it "should contain Ten of Diamonds" do
+    expect(deck.to_a).to include(Combo::Card.new(:Ten, :Diamonds, 10))
+  end
 end
